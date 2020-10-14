970aa6a551cf5af12ceae9db5f15e2fd4717bf0f
hunterlarco <hunter@larcolabs.com>
2020-10-14 14:39:39 -0400

    navbar

---
+++ src/web/components/layout/PageHeader.vue
@@ -0,0 +1,97 @@
+<template>
+  <HorizontalLayout :class="$style.Host" :right-class="$style.Buttons">
+    <template v-slot:left>
+      <div :class="$style.Logo">
+        <img src="~@/src/web/assets/logos/Mozilla.png" />
+      </div>
+    </template>
+
+    <div :class="$style.Nav">
+      <slot name="nav"></slot>
+    </div>
+
+    <template v-slot:right>
+      <slot name="buttons"></slot>
+    </template>
+  </HorizontalLayout>
+</template>
+
+<script>
+import HorizontalLayout from '@/src/web/components/layout/HorizontalLayout';
+
+export default {
+  components: { HorizontalLayout },
+};
+</script>
+
+<style module lang="sass">
+@import '@/src/web/sass/fonts';
+@import '@/src/web/sass/sizing';
+
+.Host {
+  padding: 30px;
+}
+
+.Logo {
+  display: inline-block;
+  margin-right: 24px;
+
+  @include sizing-tablet {
+    margin-right: 16px;
+  }
+
+  @include sizing-mobile {
+    margin-right: 8px;
+  }
+
+  & > img {
+    height: 24px;
+  }
+}
+
+.Nav {
+  & > * {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  & > a {
+    @include fonts-nav-link;
+
+    padding: 0 24px;
+    color: inherit;
+    cursor: pointer;
+    text-decoration: none;
+
+    @include sizing-tablet {
+      padding: 0 16px;
+    }
+
+    @include sizing-mobile {
+      padding: 0 8px;
+    }
+  }
+}
+
+.Buttons {
+  & > * {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  & > a {
+    @include fonts-nav-button;
+
+    border-radius: 4px;
+    border: 2px solid #0060DF;
+    color: #0060DF;
+    cursor: pointer;
+    padding: 4px 24px;
+    text-decoration: none;
+
+    &:hover {
+      background: lighten(#0060DF, 50%);
+    }
+  }
+}
+</style>
--- src/web/components/pages/Landing.vue
+++ src/web/components/pages/Landing.vue
@@ -1,9 +1,27 @@
 <template>
-  <div :class="$style.Host"></div>
+  <div :class="$style.Host">
+    <VerticalRibbon max-width="1200px" centered>
+      <PageHeader>
+        <template v-slot:nav>
+          <router-link to="/top">Top</router-link>
+          <router-link to="/new">New</router-link>
+        </template>
+
+        <template v-slot:buttons>
+          <router-link to="/ask">Post a new topic</router-link>
+        </template>
+      </PageHeader>
+    </VerticalRibbon>
+  </div>
 </template>
 
 <script>
-export default {};
+import PageHeader from '@/src/web/components/layout/PageHeader';
+import VerticalRibbon from '@/src/web/components/layout/VerticalRibbon';
+
+export default {
+  components: { PageHeader, VerticalRibbon },
+};
 </script>
 
 <style module lang="sass">
