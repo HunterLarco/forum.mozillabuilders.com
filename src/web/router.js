41fce5721015ce497eca71df08f05a45a4021a3e
hunterlarco <hunter@larcolabs.com>
2020-10-15 19:22:43 -0400

    gate submit behind a login

--- src/web/router.js
+++ src/web/router.js
@@ -1,6 +1,8 @@
 import VueRouter from 'vue-router';
 
-export default new VueRouter({
+import CurrentUserStore from '@/src/web/stores/CurrentUser';
+
+const router = new VueRouter({
   mode: 'history',
   routes: [
     {
@@ -21,6 +23,12 @@ export default new VueRouter({
     {
       path: '/submit',
       component: () => import('@/src/web/components/pages/Submit'),
+      meta: {
+        auth: {
+          required: true,
+          message: 'You must be logged in to post.',
+        },
+      },
     },
 
     {
@@ -44,3 +52,23 @@ export default new VueRouter({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (
+    to.meta &&
+    to.meta.auth &&
+    to.meta.auth.required &&
+    !CurrentUserStore.state.authToken
+  ) {
+    next({
+      path: '/login',
+      query: {
+        info: to.meta.auth.message,
+      },
+    });
+  } else {
+    next();
+  }
+});
+
+export default router;
