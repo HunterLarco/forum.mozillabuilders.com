590c1bc4f5fbbb7ab43f132cf065b928ea28bf41
hunterlarco <hunter@larcolabs.com>
2020-10-19 17:07:03 -0400

    remove old cron guard

--- src/server/endpoints/cron/rankHotPosts.js
+++ src/server/endpoints/cron/rankHotPosts.js
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 import JsonEndpoint from '@/src/server/helpers/net/JsonEndpoint';
-import assertCronJob from '@/src/server/helpers/net/assertCronJob';
 
 import * as PostTable from '@/src/server/firestore/Post';
 
@@ -10,8 +9,6 @@ const RequestSchema = Joi.object({});
 const ResponseSchema = Joi.object({});
 
 async function handler(environment, request, headers) {
-  assertCronJob(headers);
-
   await PostTable.updateHotRank(environment);
 
   return {};
--- src/server/helpers/net/assertCronJob.js
+++
@@ -1,9 +0,0 @@
-export default function assertCronJob(headers) {
-  if (headers['x-appengine-cron'] != 'true') {
-    throw {
-      httpErrorCode: 401,
-      name: 'Unauthorized',
-      message: 'Request must include cron job header',
-    };
-  }
-}
