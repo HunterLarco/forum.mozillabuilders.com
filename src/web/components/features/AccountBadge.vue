3e28fe513c5c35563a3bed4a0f12434dc17e8ec7
hunterlarco <hunter@larcolabs.com>
2020-12-23 14:33:16 -0500

    logout popover

--- src/web/components/features/AccountBadge.vue
+++ src/web/components/features/AccountBadge.vue
@@ -1,26 +1,75 @@
 <template>
   <div :class="$style.Host">
-    <router-link to="/account/settings">
-      <ElementIcon :class="$style.UserIcon" name="user" />
-    </router-link>
+    <ElementPopover placement="bottom" trigger="click" v-model="open_">
+      <ElementIcon
+        :class="$style.UserIcon"
+        :name="open_ ? 'user-solid' : 'user'"
+        slot="reference"
+      />
+
+      <div :class="$style.Popover">
+        <div :class="$style.Title">Account</div>
+
+        <router-link to="/account/settings" :class="$style.Button"
+          >Settings</router-link
+        >
+        <router-link to="/logout" :class="$style.Button">Log Out</router-link>
+      </div>
+    </ElementPopover>
   </div>
 </template>
 
 <script>
 import ElementIcon from '@/vendor/element-ui/Icon';
+import ElementPopover from '@/vendor/element-ui/Popover';
 
 export default {
-  components: { ElementIcon },
+  components: { ElementIcon, ElementPopover },
+
+  data() {
+    return {
+      open_: false,
+    };
+  },
 };
 </script>
 
 <style module lang="sass">
+@import '@/src/web/sass/fonts';
+
 .Host {
+  display: inline-block;
 }
 
 .UserIcon {
   color: #FFF;
   cursor: pointer;
   font-size: 24px;
+  outline: none;
+}
+
+.Popover {
+  min-width: 350px;
+}
+
+.Title {
+  @include fonts-post-title;
+
+  margin: 10px;
+}
+
+.Button {
+  @include fonts-nav-button;
+
+  color: #848484;
+  cursor: pointer;
+  display: block;
+  padding: 10px;
+  text-align: center;
+  text-decoration: underline;
+
+  &:hover {
+    color: #000;
+  }
 }
 </style>
